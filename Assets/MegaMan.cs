24f08ad8cdc8c4bad45ee0122cbd0e5a287fe645
skarl3 <skarl@umich.edu>
2015-01-24 18:27:05 -0500

    working jump and animation

--- Assets/MegaMan.cs
+++ Assets/MegaMan.cs
@@ -6,15 +6,15 @@ public class MegaMan : MonoBehaviour {
 	
 	public Vector3	vel;
 	public bool		grounded = false;
-	private bool facingRight, inAir;
+	private bool facingRight, jumping;
 	Animator anim;
-	Vector3 front, right_face, left_face;
 	
 	public float	hSpeed = 1.5f;
 	public float	acceleration = 5.4f;
-	public float	jumpVel = 100f;
+	public float	jumpVel = 10f;
 	public float	airSteeringAmt = 1f;
-	
+	float starttime;
+	public float air_time = .3f;
 	public float	airMomentumX = 1; // 0 for no momentum (i.e. 100% drag), 1 for total momentum
 	public float	groundMomentumX = 0.1f;
 	
@@ -23,13 +23,8 @@ public class MegaMan : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		peo = GetComponent<PE_Obj>();
-		
-		inAir = false;
+		jumping = false;
 		anim = GetComponent<Animator> ();
-		
-		// set front to the very front edge of the level
-		// will be compared against to dertmine Mega Man's direction
-		front = GameObject.Find ("StartWall").transform.position;
 	}
 	
 	// Update is called once per frame
@@ -44,19 +39,30 @@ public class MegaMan : MonoBehaviour {
 
 		anim.SetFloat ("speed", Mathf.Abs (vel.x));
 
+		// set direction to face
 		if (vel.x > 0)transform.eulerAngles = new Vector3 (0, 0, 0);
 		if (vel.x < 0)transform.eulerAngles = new Vector3 (0, 180, 0);
 	
 
-		// Jumping with A (which is x or .)
-		float vY = (Input.GetAxis ("Vertical"));
 
-		if (Input.GetKeyUp(KeyCode.X) || Input.GetKeyUp(KeyCode.Period)) {
-			if (grounded) {
-				vel.y = vY * jumpVel;
-				peo.ground = null; // Jumping will set ground = null
-			}		
+		//jump
+		if ((Input.GetKeyDown (KeyCode.X) || Input.GetKeyDown (KeyCode.Period)) && grounded){
+			peo.ground = null; // Jumping will set ground = null
+			jumping = true;
+			starttime = Time.time;
 		}
+		if (jumping && ((starttime + air_time) < Time.time)) {
+			jumping = false;
+		}
+		if (Input.GetKeyUp (KeyCode.X) || Input.GetKeyUp (KeyCode.Period)){
+			jumping = false;
+		}
+		if (((Input.GetKey (KeyCode.X) || Input.GetKey (KeyCode.Period))) && jumping) {
+			vel.y = jumpVel;
+		}
+		// jump 
+
+		anim.SetBool ("on_ground",grounded);
 		peo.vel = vel;
 		
 	}
