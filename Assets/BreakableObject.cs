983c0e3e9ee86a43a7242d5574bbf27b6ea54ff4
Matthew Stephens <mfstep@umich.edu>
2015-01-31 19:30:11 -0500

    Work on custom level, new intro screen, and fixed health bar

---
+++ Assets/BreakableObject.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BreakableObject : MonoBehaviour {
+	public int totalHitPoints = 5;
+	public int hitPoints = 5;
+
+	// Use this for initialization
+	void Start () {
+		hitPoints = 5;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (hitPoints == 0) {
+			PhysEngine.objs.Remove(this.GetComponent<PE_Obj>());
+			Destroy(gameObject);
+		}
+	}
+
+	public void damage() {
+		if (hitPoints > 0) {
+			hitPoints -= 1;
+			Color textureColor = gameObject.renderer.material.color;
+			textureColor.a -= 0.20f;
+			gameObject.renderer.material.color = textureColor;
+		}
+	}
+}
--- Assets/HealthBar.cs
+++ Assets/HealthBar.cs
@@ -6,13 +6,14 @@ public class HealthBar : MonoBehaviour {
 
 	public GameObject healthbarUnitPrefab;
 	public Stack<GameObject> healthUnits;
-	public int numHealthUnits = 28;
+	public int totalHealthUnits = 28;
 	public float distanceBetweenUnits = 3f;
+	public float topUnitPosition = -43.5f;
 
 	// Use this for initialization
 	void Start () {
 		healthUnits = new Stack<GameObject>();
-		for (int i = 0; i < numHealthUnits; ++i) {
+		for (int i = 0; i < totalHealthUnits; ++i) {
 			increaseByOne();
 		}
 	}
@@ -22,27 +23,51 @@ public class HealthBar : MonoBehaviour {
 	}
 
 	public void increaseByOne() {
-		if (healthUnits.Count >= numHealthUnits) return;
+		if (healthUnits.Count >= totalHealthUnits) return;
 
 		GameObject hb = GameObject.Find ("Health Bar");
 
-		// Get the distance from top health unit to bottom of bar
-		float offset = -(hb.GetComponent<RectTransform>().sizeDelta.y / 4.0f) + (distanceBetweenUnits * healthUnits.Count);
-
 		// create and add a new health unit to bar
 		GameObject go = Instantiate(healthbarUnitPrefab) as GameObject;
-		go.transform.SetParent(hb.transform);
-		Vector3 temp = go.transform.position;
-		temp = this.transform.position;
-		temp.y += offset;
-		go.transform.position = temp;
+		go.transform.SetParent(hb.transform, false);
+
+
+		Vector2 temp = go.GetComponent<RectTransform> ().anchoredPosition;
+		temp.y = topUnitPosition + distanceBetweenUnits;
+		topUnitPosition = temp.y;
+		go.GetComponent<RectTransform> ().anchoredPosition = temp;
 		healthUnits.Push(go);
 	}
 
 	public void decreaseByOne() {
 		if (healthUnits.Count > 0) {
 			GameObject removed = healthUnits.Pop();
+			topUnitPosition = removed.GetComponent<RectTransform> ().anchoredPosition.y - distanceBetweenUnits;
 			Destroy(removed);
+		}
+	}
+
+	public void increaseByAmount(int amount) {
+		for (int i = 0; i < amount; ++i) {
+			increaseByOne();
+		}
+	}
+
+	public void decreaseByAmount(int amount) {
+		for (int i = 0; i < amount; ++i) {
+			decreaseByOne();
+		}
+	}
+
+	public void empty() {
+		while (healthUnits.Count != 0) {
+			decreaseByOne();
+		}
+	}
+
+	public void fill() {
+		for (int i = 0; i < totalHealthUnits; ++i) {
+			increaseByOne();
 		}
 	}
 }
